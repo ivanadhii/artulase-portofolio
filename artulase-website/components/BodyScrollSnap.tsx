63e2c9afd828280e9d5d168701bb351e4b13f6d0
Ivan Adhi <ivanadhi@admins-MacBook-Air-2.local>
2026-03-14 17:40:55 +0700

    update

--- artulase-website/components/BodyScrollSnap.tsx
+++ artulase-website/components/BodyScrollSnap.tsx
@@ -5,6 +5,7 @@ export default function BodyScrollSnap() {
   useEffect(() => {
     document.documentElement.style.scrollSnapType = 'y mandatory'
     document.documentElement.style.overflowY = 'scroll'
+    window.scrollTo({ top: 0, behavior: 'instant' })
     return () => {
       document.documentElement.style.scrollSnapType = ''
       document.documentElement.style.overflowY = ''
